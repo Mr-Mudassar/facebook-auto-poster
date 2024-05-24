a1d6b6ab126e2f40fc0937b3220a1b1a23d8a354
Mr-Mudassar <mudassarmuhammad776@gmail.com>
2024-05-24 19:41:07 +0500

    General code setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const StartPosting = async () => {
+    console.log("Starting Posting");
+
+    const PageId = 100353242098913;
+    const AccessToken = process.env.REACT_APP_ACCESS_TOKEN;
+    console.log(AccessToken);
+
+    try {
+      const response = await fetch(
+        `https://graph.facebook.com/v20.0/${PageId}/feed`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + AccessToken,
+          },
+          body: JSON.stringify({
+            message: "Hello World",
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      console.log(data, "response from API");
+    } catch (error) {
+      console.error("There was an error!", error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={StartPosting}>Starting Posting</button>
+      {/* <button onClick={"StopPosting"}>Stop Posting</button> */}
     </div>
   );
 }
